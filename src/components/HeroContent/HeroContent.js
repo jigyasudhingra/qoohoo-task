94fb92784a3afe8d3da0b9ef78b7d6118d987465
jigyasudhingra <68184200+jigyasudhingra@users.noreply.github.com>
2023-01-24 12:39:33 +0530

    CSS code cleanup from HeroContent component

--- src/components/HeroContent/HeroContent.js
+++ src/components/HeroContent/HeroContent.js
@@ -3,18 +3,12 @@ import "./HeroContent.css";
 import MAIN_HERO_IMG from "../../assets/MainProfile.jpg";
 import FilledCircle from "../FilledCircle";
 import RIGHT_ARROW from "../../assets/RightArrow.png";
-import BLUE_TICK from "../../assets/BlueTick.png";
 
 const HeroContent = () => {
   return (
     <div className="hero-content">
       <div className="hero-main-content">
-        <div
-          style={{
-            justifyContent: "right",
-            width: "50%",
-          }}
-        >
+        <div className="main-content">
           <img className="image" src={MAIN_HERO_IMG} alt="main-hero" />
           <div className="three-grey-circles">
             {Array(3)
@@ -30,22 +24,9 @@ const HeroContent = () => {
           </div>
         </div>
         <div className="hero-main-content-right">
-          <p
-            style={{
-              fontSize: 40,
-              fontWeight: 700,
-              marginTop: -12,
-            }}
-          >
-            <span
-              style={{
-                color: "#fb9b43",
-                fontWeight: 900,
-              }}
-            >
-              Get ready
-            </span>{" "}
-            to grow with us ⭐️
+          <p className="main-heading">
+            <span className="highlighted-heading">Get ready</span> to grow with
+            us ⭐️
           </p>
           <p
             style={{
@@ -65,13 +46,7 @@ const HeroContent = () => {
       </div>
       <div className="three-ticks">
         {threeYellowCircles.map((i, idx) => (
-          <div
-            key={i.text}
-            style={{
-              textAlign: "-webkit-center",
-              color: "white",
-            }}
-          >
+          <div key={i.text} className="three-ticks-inner">
             <FilledCircle
               key={idx + "circle"}
               circleColor="#fb9b43"
@@ -79,16 +54,6 @@ const HeroContent = () => {
               height={84}
               tick
             />
-            {/* <img
-              style={{
-                position: "absolute",
-                marginTop: -55,
-                marginLeft: -18,
-                width: "3%",
-              }}
-              src={BLUE_TICK}
-              alt="blue-tick"
-            /> */}
             <h3>{i.text}</h3>
           </div>
         ))}
